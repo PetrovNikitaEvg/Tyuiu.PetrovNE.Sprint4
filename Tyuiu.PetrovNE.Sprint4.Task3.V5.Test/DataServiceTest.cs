89adf838b7d63b2a790b74acf61f8c4a15b75db4
PetrovNikita a.k Shugizawa <silestrange@gmail.com>
2023-11-06 00:20:04 +0500

    Task3

---
+++ Tyuiu.PetrovNE.Sprint4.Task3.V5.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PetrovNE.Sprint4.Task3.V5.Lib;
+
+namespace Tyuiu.PetrovNE.Sprint4.Task3.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            int[,] Array= new int[5, 5] { { 3, 4, 8, 6, 3 }, { 9, 9, 3, 6, 9 },
+                { 5, 6, 7, 2, 3 }, { 3, 6, 2, 3, 8 }, { 5, 7, 2, 6, 4 } };
+            int res = ds.Calculate(Array);
+            int wait = 25;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.PetrovNE.Sprint4.Task3.V5.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PetrovNE.Sprint4.Task3.V5.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PetrovNE.Sprint4.Task3.V5.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("badcf6fa-4191-41bc-9f6d-63794876009d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
